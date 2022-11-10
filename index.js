0da8015933b9e70738f9451268061a2544d1497c
Hemanth Kumar <hemanth21may@gmail.com>
2022-11-10 23:39:04 +0530

    content change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 require('./app/routes/data.route')(app);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log(`Server listening at port ${PORT}`)
 })
